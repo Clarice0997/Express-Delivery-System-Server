fbe02adb137b05aeda31ef297ead42ddceabcdce
Clarice0997 <clarice09975@gmail.com>
2023-01-08 22:31:48 +0800

    feat: 获取用户信息接口

--- src/main/java/com/example/expressdeliverysystemserver/controller/AdminController.java
+++ src/main/java/com/example/expressdeliverysystemserver/controller/AdminController.java
@@ -4,12 +4,12 @@ import com.example.expressdeliverysystemserver.entity.Admin;
 import com.example.expressdeliverysystemserver.entity.Login;
 import com.example.expressdeliverysystemserver.service.AdminService;
 import com.example.expressdeliverysystemserver.utils.Result;
+import com.example.expressdeliverysystemserver.utils.UserRequest;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.example.expressdeliverysystemserver.utils.JWTUtils.verifyAdmin;
 
 @RestController
 @CrossOrigin
@@ -28,6 +28,25 @@ public class AdminController {
             return Result.ok(login.getCode()).data("token", login.getToken());
         } else {
             return Result.error(login.getCode()).data("message", login.getMessage());
+        }
+    }
+
+    // 获取用户信息接口
+    @ApiOperation(value = "获取用户信息")
+    @GetMapping("/getInfo")
+    public Result getInfo(){
+        // 获取token
+        String token = UserRequest.getCurrentToken();
+        // 判断token是否存在
+        if(token == null){
+            return Result.error(401).data("message", "身份认证不通过");
+        }
+        // 解码JWT
+        Admin admin = verifyAdmin(token);
+        if (admin != null) {
+            return Result.ok(200).data("data", admin);
+        } else {
+            return Result.error(401).data("message", "身份认证不通过");
         }
     }
 }
