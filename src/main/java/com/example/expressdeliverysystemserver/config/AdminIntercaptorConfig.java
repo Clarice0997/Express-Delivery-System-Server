4a53aa2e24b3eec80bbc8f1512de74d1478950e1
Clarice0997 <clarice09975@gmail.com>
2023-01-11 21:41:21 +0800

    feat: 管理员接口拦截器

--- src/main/java/com/example/expressdeliverysystemserver/config/AdminIntercaptorConfig.java
+++ src/main/java/com/example/expressdeliverysystemserver/config/AdminIntercaptorConfig.java
@@ -1,18 +1,18 @@
 package com.example.expressdeliverysystemserver.config;
 
-import com.example.expressdeliverysystemserver.interceptor.JWTInterceptor;
+import com.example.expressdeliverysystemserver.interceptor.AdminJWTInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class IntercaptorConfig implements WebMvcConfigurer {
+public class AdminIntercaptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new JWTInterceptor())
+        registry.addInterceptor(new AdminJWTInterceptor())
                 // 拦截的路径
-                .addPathPatterns("/apis/express/mail")
+                .addPathPatterns("/apis/admin/**")
                 // 排除登录接口
-                .excludePathPatterns("/system/user/**");
+                .excludePathPatterns("/system/admin/login");
     }
 }
---
+++ src/main/java/com/example/expressdeliverysystemserver/interceptor/AdminJWTInterceptor.java
@@ -0,0 +1,36 @@
+package com.example.expressdeliverysystemserver.interceptor;
+
+import com.alibaba.druid.util.StringUtils;
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.example.expressdeliverysystemserver.utils.JWTUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdminJWTInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // 获取JWT
+        String token = request.getHeader("Authorization");
+        // 判断JWT是否为空
+        if (StringUtils.isEmpty(token)) {
+            return false;
+        }
+        try {
+            // 验证JWT是否合法
+            JWTUtils.verifyAdmin(token);
+        } catch (SignatureVerificationException e) {
+            return false;
+        } catch (TokenExpiredException e) {
+            return false;
+        } catch (AlgorithmMismatchException e) {
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
